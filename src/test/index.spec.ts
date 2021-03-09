1e17d6bb38c0e459e933116050eeb046ed1d5172
hsan8 <dhaouadi002@gmail.com>
2021-03-09 23:51:07 +0300

    add new package

---
+++ src/test/index.spec.ts
@@ -0,0 +1,5 @@
+describe("Test", function () {
+    it("Testing", function () {
+        console.log("done!")
+    });
+})
---
+++ src/test/server/server-runs.spec.ts
@@ -0,0 +1,5 @@
+describe("server check", function () {
+    it("server created ithout error", function () {
+        console.log("done!")
+    });
+})
